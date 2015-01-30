abdffb7dfd07d98655fda65f2d4c835e186f11d1
luanzhikun <972663246@qq.com>
2015-01-30 22:07:50 +0800

    项目提交

---
+++ src/regularExpression/Replacing.java
@@ -0,0 +1,14 @@
+package regularExpression;
+
+public class Replacing 
+{
+	static String s = Splitting.knights;
+	public static void main(String[] args)
+	{
+		System.out.println(s.replaceFirst("f\\w+","located"));
+		System.out.println(s.replaceAll("shrubbery|tree|herring","banana"));
+		System.out.println(s.replaceAll("a|e|i|o|u", "_"));
+		
+	}
+
+}
